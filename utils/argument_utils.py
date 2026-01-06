005cef41d951f685ed3059159b9769362fcbcd88
gutaosky <1093656744@qq.com>
2026-01-07 00:06:38 +0800

    modify argumentsUtil

--- utils/argument_utils.py
+++ utils/argument_utils.py
@@ -9,13 +9,11 @@ class ArgumentUtils:
         self.parser.add_argument('--config', type=str, default='config.yaml', help='项目的整体配置文件')
         self.parser.add_argument('--model_type', type=str,  default='OpenAIModel',
                                  choices=['GLMModel', 'OpenAIModel'], help='选择OpenAI还是GLM的模型')
-        self.parser.add_argument('--glm_model_url', type=str, help='ChatGLM的模型访问地址')
-
-        self.parser.add_argument('--timeout', type=str, help='API接口请求超时时间')
-        self.parser.add_argument('--openai_model', type=str, help='OPENAI中所使用的模型名字')
-        self.parser.add_argument('--openai_api_key', type=str, help='OPENAI中的api_key')
-        self.parser.add_argument('--book', type=str, help='需要翻译的书籍所属的文件路径')
+        self.parser.add_argument('--model_name', type=str, help='大语言模型名')
+        self.parser.add_argument('--input_file', type=str, help='需要翻译的文件所属路径')
         self.parser.add_argument('--file_format', type=str, help='翻译之后生成的文件格式')
+        self.parser.add_argument('--source_language', type=str, help='书籍的源语言')
+        self.parser.add_argument('--target_language', type=str, help='翻译后的目标语言')
 
     def parse_argument(self):
         '''
