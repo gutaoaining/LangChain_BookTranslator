fa4640c867f33d6d3644f6af006dbd8d59f162f1
gutaosky <1093656744@qq.com>
2026-01-10 23:46:18 +0800

    add init method of OpenAIModel

--- ai_model/openai_model.py
+++ ai_model/openai_model.py
@@ -1,12 +1,18 @@
 # 作者：顾涛
 # 创建时间：2026/1/10
 from ai_model.model import Model
+from langchain_openai import (ChatOpenAI)
 
 
 class OpenAIModel(Model):
+
+    def __init__(self, model_name: str, api_key: str):
+        self.model_name = model_name
+        self.api_key = api_key
 
     def create_llm(self):
         """
         初始化openai的大语言模型对象
         :return:
         """
+        model = ChatOpenAI(model=self.model_name,api_key=self.api_key,temperature=0)
