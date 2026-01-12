b3c45b1e5de7c9f272be532f1e1b88c76918e5f0
gutaosky <1093656744@qq.com>
2026-01-12 23:27:48 +0800

    add TranslatorChain

---
+++ translator/translator_chain.py
@@ -0,0 +1,11 @@
+# 作者：顾涛
+# 创建时间：2026/1/12
+from ai_model.model import Model
+
+
+class TranslatorChain:
+    """
+    负责调用langchain来完成文本的翻译
+    """
+    def __init__(self, model: Model):
+        pass
